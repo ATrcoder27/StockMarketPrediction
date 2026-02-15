bf7c7399cf91717cae4b08ee238a14b87635fe57
Aicod3r4 <vivaan.dalal.official@gmail.com>
2026-02-15 16:04:12 +0530

    Update ai.py

--- ai.py
+++ ai.py
@@ -28,44 +28,49 @@ for i in range(1200):
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, shuffle=False)
 
+baseline_value = np.mean(Y_test)
+baseline_pred = np.full(len(Y_test), baseline_value)
+baselineMSQ = mean_squared_error(Y_test, baseline_pred)
+print("Baseline MSQ :", baselineMSQ)
+
 lin = linear_model.LinearRegression()
 lin.fit(X_train, Y_train)
 linPred = lin.predict(X_test)
-linMSQ = mean_squared_error(Y_test, linPred)
+linMSE = mean_squared_error(Y_test, linPred)
 linMAE = mean_absolute_error(Y_test, linPred)
-print("Linear MSQ: ", linMSQ)
+print("Linear MSE: ", linMSE)
 print("Linear MAE: ", linMAE)
 
 dt = DecisionTreeRegressor(max_depth=10)
 dt.fit(X_train, Y_train)
 dtPred = dt.predict(X_test)
-dtMSQ = mean_squared_error(Y_test, dtPred)
+dtMSE = mean_squared_error(Y_test, dtPred)
 dtMAE = mean_absolute_error(Y_test, dtPred)
-print("Decision Tree MSQ: ", dtMSQ)
+print("Decision Tree MSE: ", dtMSE)
 print("Decision Tree MAE: ", dtMAE)
 
 rf = RandomForestRegressor(max_depth=10, n_estimators=50)
 rf.fit(X_train, Y_train)
 rfPred = rf.predict(X_test)
-rfMSQ = mean_squared_error(Y_test, rfPred)
+rfMSE = mean_squared_error(Y_test, rfPred)
 rfMAE = mean_absolute_error(Y_test, rfPred)
-print("Random Forest MSQ: ", rfMSQ)
+print("Random Forest MSE: ", rfMSE)
 print("Random Forest MAE: ", rfMAE)
 
 mlp = MLPRegressor(max_iter=1000)
 mlp.fit(X_train, Y_train)
 mlpPred = mlp.predict(X_test)
-mlpMSQ = mean_squared_error(Y_test, mlpPred)
+mlpMSE = mean_squared_error(Y_test, mlpPred)
 mlpMAE = mean_absolute_error(Y_test, mlpPred)
-print("Multi-Layer Perception MSQ: ", mlpMSQ)
+print("Multi-Layer Perception MSE: ", mlpMSE)
 print("Multi-Layer Perception MAE: ", mlpMAE)
 
 knn = neighbors.KNeighborsRegressor(n_neighbors=10)
 knn.fit(X_train, Y_train)
 knnPred = knn.predict(X_test)
-knnMSQ = mean_squared_error(Y_test, knnPred)
+knnMSE = mean_squared_error(Y_test, knnPred)
 knnMAE = mean_absolute_error(Y_test, knnPred)
-print("K Nearest Neighbors MSQ: ", knnMSQ)
+print("K Nearest Neighbors MSE: ", knnMSE)
 print("K Nearest Neighbors MAE: ", knnMAE)
 
 print("")
@@ -73,14 +78,14 @@ print("")
 print("")
 
 avgPred = (linPred + dtPred + rfPred + mlpPred + knnPred)/5
-avgMSQ = mean_squared_error(Y_test, avgPred)
-print("Average MSQ: ", avgMSQ)
+avgMSE = mean_squared_error(Y_test, avgPred)
+print("Average MSE: ", avgMSE)
 
-linCoeff = 1/linMSQ
-dtCoeff = 1/dtMSQ
-rfCoeff = 1/rfMSQ
-mlpCoeff = 1/mlpMSQ
-knnCoeff = 1/knnMSQ
+linCoeff = 1/linMSE
+dtCoeff = 1/dtMSE
+rfCoeff = 1/rfMSE
+mlpCoeff = 1/mlpMSE
+knnCoeff = 1/knnMSE
 
 totalCoeff = linCoeff + dtCoeff + rfCoeff + mlpCoeff + knnCoeff
 
@@ -100,60 +105,81 @@ knnWeight = knnCoeff/totalCoeff
 print("K Nearest Neighbors Weight: ", knnWeight)
 
 totalPred = ((linPred * linWeight) + (dtPred * dtWeight) + (rfPred * rfWeight) + (mlpPred * mlpWeight) + (knnPred * knnWeight))
-totalMSQ = mean_squared_error(Y_test, totalPred)
-print("Total MSQ: ", totalMSQ)
+totalMSE = mean_squared_error(Y_test, totalPred)
+print("Total MSE: ", totalMSE)
 
-predMSQ = [0]*1260
+predMSE = [0]*1260
 for i in range(1250):
-    predMSQ[i] = ((lin.predict([X[i]]) * linWeight) + (dt.predict([X[i]]) * dtWeight) + (rf.predict([X[i]]) * rfWeight) + (mlp.predict([X[i]]) * mlpWeight) + (knn.predict([X[i]]) * knnWeight))
-    
+    predMSE[i] = ((lin.predict([X[i]]) * linWeight) + (dt.predict([X[i]]) * dtWeight) + (rf.predict([X[i]]) * rfWeight) + (mlp.predict([X[i]]) * mlpWeight) + (knn.predict([X[i]]) * knnWeight))
 
-wlin = 1/5
-wdt = 1/5
-wrf = 1/5
-wmlp = 1/5
-wknn = 1/5
+eps = 1e-4
+n = len(X_test)
+predRoll = np.zeros(n)
 
-for i in range(len(X_test)):
-  linError = abs(linPred[i] - Y_test[i])
-  dtError = abs(dtPred[i] - Y_test[i])
-  rfError = abs(rfPred[i] - Y_test[i])
-  mlpError = abs(mlpPred[i] - Y_test[i])
-  knnError = abs(knnPred[i] - Y_test[i])
+wlin = wdt = wrf = wmlp = wknn = wlstm = 1/6
 
-  linInv  = wlin/(linError+0.0001)
-  dtInv = wdt/(dtError+0.0001)
-  rfInv = wrf/(rfError+0.0001)
-  mlpInv = wmlp/(mlpError+0.0001)
-  knnInv = wknn/(knnError+0.0001)
-  totalInv = linInv + dtInv + rfInv + mlpInv + knnInv
+for i in range(n):
+    linError  = abs(linPred[i]  - Y_test[i])
+    dtError   = abs(dtPred[i]   - Y_test[i])
+    rfError   = abs(rfPred[i]   - Y_test[i])
+    mlpError  = abs(mlpPred[i]  - Y_test[i])
+    knnError  = abs(knnPred[i]  - Y_test[i])
+    lstmError = abs(lstmPred[i] - Y_test[i])
 
-  wlin = linInv/totalInv
-  wdt = dtInv/totalInv
-  wrf = rfInv/totalInv
-  wmlp = mlpInv/totalInv
-  wknn = knnInv/totalInv
-print(wlin, wdt, wrf, wmlp, wknn)
+    linInv  = wlin  / (linError  + eps)
+    dtInv   = wdt   / (dtError   + eps)
+    rfInv   = wrf   / (rfError   + eps)
+    mlpInv  = wmlp  / (mlpError  + eps)
+    knnInv  = wknn  / (knnError  + eps)
+    lstmInv = wlstm / (lstmError + eps)
 
-predRoll = [0]*1260
-for i in range(1250):
-    predRoll[i] = ((lin.predict([X[i]]) * wlin) + (dt.predict([X[i]]) * wdt) + (rf.predict([X[i]]) * wrf) + (mlp.predict([X[i]]) * wmlp) + (knn.predict([X[i]]) * wknn))
+    totalInv = linInv + dtInv + rfInv + mlpInv + knnInv + lstmInv
+
+    wlin  = linInv  / totalInv
+    wdt   = dtInv   / totalInv
+    wrf   = rfInv   / totalInv
+    wmlp  = mlpInv  / totalInv
+    wknn  = knnInv  / totalInv
+    wlstm = lstmInv / totalInv
+
+    x_tab = np.array(X_test[i]).reshape(1, -1)
+    x_lstm = np.array(X_np[i], dtype=np.float32).reshape(1, 3, 2)
+
+    lin_p  = float(lin.predict(x_tab))
+    dt_p   = float(dt.predict(x_tab))
+    rf_p   = float(rf.predict(x_tab))
+    mlp_p  = float(mlp.predict(x_tab))
+    knn_p  = float(knn.predict(x_tab))
+    lstm_p = float(lstm.predict(x_lstm, verbose=0))
+
+    predRoll[i] = (lin_p*wlin + dt_p*wdt + rf_p*wrf + mlp_p*wmlp + knn_p*wknn + lstm_p*wlstm)
+
+print(wlin, wdt, wrf, wmlp, wknn, wlstm)
 
 print("")
 print("")
 print("")
 
 print("Initializing Game...")
+print("Initializing Game...")
 money = int(input(f"Starting Amount: {ticker.info['currency']}"))
 time = int(input("Length of Investment (in days): "))
 stocks = 0
 
 for i in range(time):
-    if predMSQ[i+1] - Y[i] > 0 and money > 1 * Y[i]:
-        money = money - (1 * Y[i])
-        stocks = stocks + 1
-    elif predMSQ[i+1] - Y[i] < 0 and stocks > 1:
-        money = money + (1 * Y[i])
-        stocks = stocks - 1
-money = (stocks * Y[time]) + money
+    pred_next = float(np.ravel(predMSQ[i+1])[0])
+    price_now = float(np.ravel(Y[i])[0])
+
+    k=round(int((pred_next/price_now)*6),0)
+
+    if (pred_next - price_now) > 0 and money > k * price_now:
+        money -= k * price_now
+        stocks += k
+    elif (pred_next - price_now) < 0 and stocks >= k:
+        money += k * price_now
+        stocks -= k
+
+final_price = float(np.ravel(Y[time])[0])
+money = money + stocks * final_price
+
 print(f"Final Amount: {ticker.info['currency']}", money)
